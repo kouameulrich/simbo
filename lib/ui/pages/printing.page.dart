a15c8bf6b241cf23e2412ace3448a23cebcbaeda
ulrichkpuame <116578039+ulrichkpuame@users.noreply.github.com>
2023-12-18 12:07:02 +0000

    Commit from Ricardo

--- lib/ui/pages/printing.page.dart
+++ lib/ui/pages/printing.page.dart
@@ -1,8 +1,12 @@
-import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:simbo_mobile/ui/pages/collecte.page.dart';
-import 'dart:typed_data';
+import 'package:esc_pos_bluetooth/esc_pos_bluetooth.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
 
 class PrintingPage extends StatefulWidget {
   final pw.Document docPage;
@@ -13,19 +17,28 @@ class PrintingPage extends StatefulWidget {
 }
 
 class _PrintingPageState extends State<PrintingPage> {
-  List<BluetoothDevice> device = [];
-  BluetoothDevice? selectedDevices;
-  BlueThermalPrinter printer = BlueThermalPrinter.instance;
+  PrinterBluetoothManager _printerManager = PrinterBluetoothManager();
+  List<PrinterBluetooth> _devices = [];
+  String? _devicesMsg;
+
+  BluetoothManager bluetoothManager = BluetoothManager.instance;
 
   @override
   void initState() {
+    bluetoothManager.state.listen((val) {
+      if (!mounted) return;
+      if (val == 12) {
+        initPrinter();
+        print('on');
+      } else if (val == 10) {
+        print('off');
+        setState(() {
+          _devicesMsg = 'Bluetooth Disconnect!';
+        });
+      }
+    });
+    print(widget.docPage);
     super.initState();
-    getDevices();
-  }
-
-  void getDevices() async {
-    device = await printer.getBondedDevices();
-    setState(() {});
   }
 
   @override
@@ -43,59 +56,95 @@ class _PrintingPageState extends State<PrintingPage> {
         centerTitle: true,
         title: const Text('Preview Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButton<BluetoothDevice>(
-              value: selectedDevices,
-              hint: Text('Select printer'),
-              items: device
-                  .map((e) => DropdownMenuItem(
-                        value: e,
-                        child: Text(e.name.toString()),
-                      ))
-                  .toList(),
-              onChanged: (device) {
-                selectedDevices = device;
+      body: _devices.isEmpty
+          ? Center(
+              child: Text(_devicesMsg ?? ''),
+            )
+          : ListView.builder(
+              itemCount: _devices.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_devices[index].name.toString()),
+                  subtitle: Text(_devices[index].address.toString()),
+                  onTap: () {
+                    _startPrint(_devices[index]);
+                  },
+                );
               },
             ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  printer.connect(selectedDevices!);
-                },
-                child: Text('Connect')),
-            ElevatedButton(
-                onPressed: () {
-                  printer.disconnect();
-                },
-                child: Text('Disconnect')),
-            ElevatedButton(
-              onPressed: () async {
-                if (selectedDevices != null) {
-                  await printer.connect(selectedDevices!);
+    );
+  }
 
-                  if ((await printer.isConnected)!) {
-                    final Uint8List pdfBytes = await widget.docPage.save();
-                    await printer.writeBytes(pdfBytes);
-                    await printer.paperCut();
-                    print('------------  DONNÉES  -----------');
-                    print(pdfBytes);
-                  } else {
-                    print('Erreur de connexion à l\'imprimante');
-                  }
-                } else {
-                  print('Aucune imprimante sélectionnée');
-                }
-              },
-              child: Text('Imprimer'),
-            ),
-          ],
-        ),
+  void initPrinter() {
+    _printerManager.startScan(Duration(seconds: 2));
+    _printerManager.scanResults.listen((val) {
+      if (!mounted) return;
+
+      setState(() {
+        _devices = val;
+      });
+      print(_devices);
+      if (_devices.isEmpty) {
+        setState(() {
+          _devicesMsg = 'No Devices';
+        });
+      }
+
+      print(val);
+    });
+  }
+
+  Future<void> _startPrint(PrinterBluetooth printer) async {
+    _printerManager.selectPrinter(printer);
+    final result =
+        await _printerManager.printTicket(_ticket(PaperSize.mm80) as List<int>);
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              content: Text(result.msg),
+            ));
+  }
+
+  Future<Ticket> _ticket(PaperSize paper) async {
+    final ticket = Ticket(paper);
+    int total = 0;
+    // ticket.text('test');
+    ticket.text(
+      'SIMBO',
+      styles: PosStyles(
+        align: PosAlign.center,
+        height: PosTextSize.size2,
+        width: PosTextSize.size2,
       ),
     );
+
+    Uint8List result = await widget.docPage.save();
+    for (var i = 0; i < result.length; i++) {
+      total += result[i];
+    }
+    ticket.cut();
+    return ticket;
+  }
+
+  @override
+  void dispose() {
+    _printerManager.stopScan();
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
+
+class Ticket {
+  // Vos méthodes et propriétés ici
+  Ticket(PaperSize paper) {
+    // Initialisation de la classe Ticket
+  }
+
+  void text(String text, {required PosStyles styles}) {
+    // Implémentation de la méthode text
+  }
+
+  void cut() {
+    // Implémentation de la méthode cut
   }
 }
